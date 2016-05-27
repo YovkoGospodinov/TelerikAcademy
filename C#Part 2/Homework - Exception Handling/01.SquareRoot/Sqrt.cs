1ffe5a449c27d543d11a9df8276872edfd03f77e
Yovko Gospodinov <yovko.gospodinov@gmail.com>
2016-05-27 22:21:17 +0200

    Adding Exceptions Handling homework

---
+++ C#Part 2/Homework - Exception Handling/01.SquareRoot/Sqrt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _01.SquareRoot
+{
+    class Sqrt
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                double number = double.Parse(input);
+                if (number <= 0)
+                {
+                    throw new FormatException("Invalid number");
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine("{0:F3}", result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            } 
+        }
+    }
+}
---
+++ C#Part 2/Homework - Exception Handling/02.EnterNumbers/Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EnterNumbers
+{
+    class Numbers
+    {
+        static void Main()
+        {
+            int[] sequenceNumbers = new int[12] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100 };
+            int lastNumber = 1;
+            bool isSequence = true;
+            for (int i = 1; i < 11; i++)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    int number = int.Parse(input);
+                    if (lastNumber >= number)
+                    {
+                        throw new FormatException("Exception");
+                    }
+                    lastNumber = number;
+                    sequenceNumbers[i] = number;
+                }
+                catch (FormatException)
+                {
+                    isSequence = false;
+                }
+            }
+            if (isSequence)
+            {
+                Console.WriteLine(string.Join(" < ", sequenceNumbers));
+            }
+            else
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+    }
+}
