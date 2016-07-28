333150d1e42e25eaf0d85cc861260e4ae6233149
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-07-28 10:38:13 +0300

    update the solution

--- JavaScript UI & DOM/Exercise - DOM Operations/task-1.js
+++ JavaScript UI & DOM/Exercise - DOM Operations/task-1.js
@@ -4,12 +4,21 @@ function solve() {
 
         var docFrag = document.createDocumentFragment();
 
-        if (element === null || contents === null) {
+        if (element === null || contents === null || !Array.isArray(contents)) {
             throw new ExceptionInformation("Exception");
+        }
+
+        for (var i = 0; i < contents.length; i += 1) {
+            if (typeof contents[i] !== "string" && typeof contents[i] !== "number") {
+                throw new ExceptionInformation("Exception");
+            }
         }
 
         if (typeof element === "string") {
             element = document.getElementById(element);
+            if (element === null) {
+                throw new ExceptionInformation("Exception");
+            }
 
         } else if (element instanceof HTMLElement) {
             element = element;
@@ -20,8 +29,8 @@ function solve() {
         element.innerHTML = "";
         var div = document.createElement("div");
 
-        for (var i = 0; i < contents.length; i += 1) {
-            var el = contents[i];
+        for (var j = 0; j < contents.length; j += 1) {
+            var el = contents[j];
             var divToAdd = div.cloneNode(true);
             divToAdd.innerHTML = el;
             docFrag.appendChild(divToAdd);
