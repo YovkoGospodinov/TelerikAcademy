b209553bb33611091304e890cedf74b8d1c6b39b
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-08-02 15:34:16 +0300

    adding solution for workshop

---
+++ Unit Testing/WorkShop/Solution/Cosmetics.Test/CommandTest.cs
@@ -0,0 +1,94 @@
+﻿namespace Cosmetics.Test
+{
+    using Cosmetics.Engine;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class CommandTest
+    {
+        [Test]
+        public void ParseShouldReturnNewCommandWhenInputStringIsInValidFormat()
+        {
+            Assert.IsInstanceOf(typeof(Command), Command.Parse("some string"));
+        }
+
+        [Test]
+        public void ParseShouldSetCorrectValueToThePropertyNameOfTheNewlyCreateObjectWhenInputStringIsValid()
+        {
+            // Arrange
+            var expectedName = "some";
+
+            // Act
+            var actualName = Command.Parse("some string").Name;
+
+            // Assert
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void ParseShouldSetCorrectValueToThePropertyParametersOfTheNewlyCreateObjectWhenInputStringIsValid()
+        {
+            // Arrange
+            IList<string> expectedParameters = new List<string>()
+            {
+                "string",
+                "value"
+            };
+
+            // Act
+            var actualParameters = Command.Parse("some string value").Parameters;
+
+            // Assert
+            Assert.AreEqual(expectedParameters, actualParameters);
+        }
+
+        [Test]
+        public void ParseShouldThrowArgumentNullExceptionWhenTheInputStringThatRepresentCommandNameIsEmpty()
+        {
+            // Arrange
+            string expectedName = String.Empty;
+
+            // Act and Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => Command.Parse(expectedName));
+            StringAssert.Contains("Name", exception.Message);
+        }
+
+        [Test]
+        public void ParseShouldThrowArgumentNullExceptionWhenTheInputStringThatRepresentCommandNameIsNull()
+        {
+            // Arrange
+            string expectedName = null;
+
+            // Act and Assert
+            var exception = Assert.Throws<NullReferenceException>(() => Command.Parse(expectedName));
+        }
+
+        [Test]
+        public void ParseShouldThrowArgumentNullExceptionWhenTheInputStringThatRepresentCommandParametersIsNull()
+        {
+            // Arrange
+            var input = $"{"name"} {null}";
+
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => Command.Parse(input));
+
+            // Assert
+            StringAssert.Contains("List", exception.Message);
+        }
+
+        [Test]
+        public void ParseShouldThrowArgumentNullExceptionWhenTheInputStringThatRepresentCommandParametersIsEmpty()
+        {
+            // Arrange
+            var input = $"{"name"} {String.Empty}";
+
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => Command.Parse(input));
+
+            // Assert
+            StringAssert.Contains("List", exception.Message);
+        }
+    }
+}
---
+++ Unit Testing/WorkShop/Solution/Cosmetics.Test/ValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿namespace Cosmetics.Test
+{
+    using System;
+    using Cosmetics.Common;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ValidatorTest
+    {
+        [Test]
+        public void CheckIfNullShouldThrowNullReferenceExceptionIfTheParameterObjIsNull()
+        {
+            Assert.Throws<NullReferenceException>(() => Validator.CheckIfNull(null));
+        }
+
+        [Test]
+        public void CheckIfNullShouldNotThrowNullReferenceExceptionIfTheParameterObjIsNotNull()
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfNull(String.Empty));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void CheckIfStringIsNullOrEmptyShouldThrowNullReferenceExceptionWhenTheParameterTextIsNullOrEmpty(string text)
+        {
+            Assert.Throws<NullReferenceException>((() => Validator.CheckIfStringIsNullOrEmpty(text)));
+        }
+
+        [Test]
+        public void CheckIfStringIsNullOrEmptyShouldNotThrowNullReferenceExceptionWhenTheParameterTextIsNotNullOrEmpty()
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfStringIsNullOrEmpty("a"));
+        }
+
+        [Test]
+        [TestCase("ca", 20, 3)]
+        [TestCase("cacacacacacacacacacaca", 20, 3)]
+        public void CheckIfStringLengthIsValidShouldThrowIndexOutOfRangeExceptionWhenLengthOfParameterTextIsLowerThanMinimumAllowedValueOrGreaterThanTheMaximumAllowed
+            (string text, int max, int min)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Validator.CheckIfStringLengthIsValid(text, max, min));
+        }
+
+        [Test]
+        [TestCase("cat", 20, 3)]
+        [TestCase("turtle", 10, 4)]
+        public void CheckIfStringLengthIsValidShouldNotThrowIndexOutOfRangeExceptionWhenLengthOfParameterTextIsValid(string text, int max, int min)
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfStringLengthIsValid(text, max, min));
+        }
+    }
+}
