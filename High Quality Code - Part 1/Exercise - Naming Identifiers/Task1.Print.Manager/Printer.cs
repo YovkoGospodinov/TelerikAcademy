015dc9b3866c6096264b07d56efa60e850e59e0d
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-08-29 00:12:03 +0300

    adding naming identifiers exercise

---
+++ High Quality Code - Part 1/Exercise - Naming Identifiers/Task1.Print.Manager/Printer.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1.Print.Manager
+{
+    using System;
+
+    public class Printer
+    {
+        public void BooleanPrint(bool booleanValue)
+        {
+            string booleanValueAsString = booleanValue.ToString();
+
+            Console.WriteLine(booleanValueAsString);
+        }
+    }
+}
---
+++ High Quality Code - Part 1/Exercise - Naming Identifiers/Task1.Print.Manager/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1.Print.Manager
+{
+    public class StartUp
+    {
+        private const int MaxCount = 6;
+
+        public static void Main()
+        {
+            var printer = new Printer();
+
+            printer.BooleanPrint(true);
+        }
+    }
+}
