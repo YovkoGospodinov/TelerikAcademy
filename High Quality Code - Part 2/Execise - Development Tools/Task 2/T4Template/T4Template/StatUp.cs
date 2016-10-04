78ccecb76825a85852b2b2b50b5aaefeb8880d13
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-10-04 23:37:42 +0300

    adding dev tool execise

---
+++ High Quality Code - Part 2/Execise - Development Tools/Task 2/T4Template/T4Template/StatUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace T4Template
+{
+    public class StatUp
+    {
+        public static readonly ILog Log = LogManager.GetLogger(typeof(StatUp));
+        public static void Main()
+        {
+            Person person = new Person("Martin", "Dimitov", 56, "Pernik");
+            Console.WriteLine(person.FirstName);
+
+            XmlConfigurator.Configure();
+            Log.Debug(person.FirstName);
+        }
+    }
+}
