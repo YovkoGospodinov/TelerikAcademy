41f9651a0b77eb3f0b22a0ccc752a40d6c9cde1b
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-10-22 10:26:13 +0300

    updating exercise with new task's solutions

---
+++ Databases/Exercise - Processing XML in .NET/Processing-XML-in-.NET/LINQQueryExtractSongs/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _06.LINQQueryExtractSongs
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string path = "../../../UsedFiles/catalog.xml";
+            ExtractSongsNameLinq(path);
+        }
+
+        private static void ExtractSongsNameLinq(string path)
+        {
+            var xml = XDocument.Load(path);
+
+            var albums = xml.Element("albums")
+                .Elements("album");
+
+            var titles = from title in albums.Descendants("title") select title.Value;
+
+            Console.WriteLine("Songs titles:");
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
